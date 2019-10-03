df01d4042c00c94df15e275acaa7392e6f9303d1
Ross W <rossw3@vt.edu>
2019-10-03 14:50:43 -0400

    Oct 3rd Commit

---
+++ 4-Va Unity Project/Assets/SliderScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    public GameObject water;
+
+    void Start()
+    {
+        
+    }
+
+    public void setSliderValue(GameObject slider)
+    {
+        water.gameObject.transform.position = new Vector3(0, slider.gameObject.GetComponent<Slider>().value, 0);
+    }
+}
