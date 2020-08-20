454bf1db5d4bc18574de6db8a6d04137d1457740
Rosscoe3 <45441750+Rosscoe3@users.noreply.github.com>
2020-08-20 15:19:28 -0400

    COMMIT

--- 4-Va Unity Project/Assets/moveMeanScript.cs
+++ 4-Va Unity Project/Assets/moveMeanScript.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using ChartAndGraph;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class moveMeanScript : MonoBehaviour
 {
     public Image mean, median, legend;
+    public GameObject bar;
     //public GameObject legend;
 
     bool fullscreen;
@@ -17,8 +21,106 @@ public class moveMeanScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //GetPositionOnGraph();
+        Run();
     }
+
+    public void GetPositionOnGraph()
+    {
+        var barChart = bar.GetComponent<BarChart>();
+
+        Vector3 bottomPosition;
+        if (barChart.GetBarTrackPosition("Group 1", "Player 1", out bottomPosition))
+        {
+            meanPos = bottomPosition;
+            meanFullPos = bottomPosition;
+
+            Debug.Log("Top Position: " + bottomPosition);
+            //topPosition now contains the top position of the bar
+        }
+        else
+        {
+            Debug.Log("NOPE");
+        }
+    }
+
+    public void Run()
+    {
+        var barChart = bar.GetComponent<BarChart>();
+
+        barChart.DataSource.StartBatch();
+        barChart.DataSource.ClearValues();
+
+        int[] barValues;
+        barValues = new int[10];
+
+
+        for (int i = 0; i < barValues.Length; i++)
+        {
+            barValues[i] = UnityEngine.Random.Range(1, 30);
+
+            string categoryName = (2011 + i).ToString();
+            barChart.DataSource.SetValue(categoryName, "Values", barValues[i]);
+
+           /* float animationTime = 1.0f; // one second;
+            barChart.DataSource.SlideValue(categoryName, "Values", barValues[i], animationTime);*/
+        }
+
+        barChart.DataSource.EndBatch();
+        meanCalculate(barValues);
+    }
+    public void meanCalculate(int[] values)
+    {
+        var barChart = bar.GetComponent<BarChart>();
+
+        float mean = 0;
+        int sum = 0;
+
+        for (int x = 0; x < values.Length; x++)
+        //sums up all the values
+        {
+            Debug.Log("value " + x.ToString() + ": " + values[x]);
+            sum += values[x];
+        }
+
+        mean = sum / values.Length;
+        Debug.Log("mean: " + mean);
+        //calculates the mean
+
+        var nearest = values.Aggregate((current, next) => Math.Abs((long)current - mean) < Math.Abs((long)next - mean) ? current : next);
+        //Debug.Log("nearest: " + nearest);
+        int index = Array.IndexOf(values, nearest);
+        //Debug.Log("Index: " + index);
+        string indexCategory = (2011+index).ToString();
+        //Debug.Log("Index Category: " + indexCategory);
+        //finds the closest value in the array to the mean at that index
+        
+        Vector3 bottomPosition;
+
+        //Debug.Log(barChart.DataSource.GetCategoryName(index));
+       // Debug.Log(barChart.DataSource.GetGroupName(0));
+
+       // Debug.Log(barChart.GetBarTrackPosition(barChart.DataSource.GetCategoryName(index), barChart.DataSource.GetGroupName(0), out bottomPosition));
+
+
+       /* if (bar.GetBarBottomPosition(indexCategory, "Values", out bottomPosition))
+        {
+            meanPos = bottomPosition;
+            meanFullPos = bottomPosition;
+
+            Debug.Log("Top Position: " + bottomPosition);
+            //topPosition now contains the top position of the bar
+        }
+        else
+        {
+            Debug.Log("FAILED, category at index:" + bar.DataSource.GetCategoryName(index) + " ");
+        }*/
+
+
+
+    }
+
 
     public void Fullscreen()
     {
